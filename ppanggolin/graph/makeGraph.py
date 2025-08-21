589b9ce85cbd9c8c42d31ae727362296d97c621f
Hanineali <hanineali.bc@gmail.com>
2025-08-22 01:28:18 +0200

    remove skipping family not found in makegraph

--- ppanggolin/graph/makeGraph.py
+++ ppanggolin/graph/makeGraph.py
@@ -184,7 +184,7 @@ def compute_neighbors_graph(
                             prev_feat, feature
                         )
                     # If the family is removed, store in temp_list
-                    if feature.family.removed or feature.family is None: #skip temprary the family not found
+                    if feature.family.removed :
                         if intergenic:
                             temp_list.append(intergenic)
                         temp_list.append(feature)
@@ -211,7 +211,7 @@ def compute_neighbors_graph(
 
                 except AttributeError:
                     raise AttributeError(
-                        "A feature does not have a GeneFamily object associated."
+                        "A feature does not have a  object associated."
                     )
                 except Exception:
                     raise Exception("Unexpected error. Please report on our github.")
